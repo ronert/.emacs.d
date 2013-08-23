da47c524e249aed5d51b16a469e4a65feac3265e
Ronert Obst <ronert.obst@gmail.com>
2013-08-23 13:13:12 +0200

    added undo region keep active from what the emacs.d

--- init-misc.el
+++ init-misc.el
@@ -181,4 +181,16 @@ indent yanked text (with prefix arg don't indent)."
 ;; activate pending-delete-mode
 (pending-delete-mode t)
 
+;; Keep region when undoing in region
+(defadvice undo (around keep-region activate)
+  (if (use-region-p)
+      (let ((m (set-marker (make-marker) (mark)))
+            (p (set-marker (make-marker) (point))))
+        ad-do-it
+        (goto-char p)
+        (set-mark m)
+        (set-marker p nil)
+        (set-marker m nil))
+    ad-do-it))
+
 (provide 'init-misc)
