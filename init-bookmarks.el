b5f4894155e43107e6633060ce5f9751d4618cf6
Ronert Obst <ronert.obst@gmail.com>
2013-10-19 21:00:58 +0200

    changed bookmark bindings

--- init-bookmarks.el
+++ init-bookmarks.el
@@ -1,7 +1,7 @@
 ;; Bookmark +
 (require-package 'bookmark+)
 (global-set-key (kbd "<f7>") 'bookmark-jump)
-(global-set-key (kbd "<f8>") 'bookmark-bmenu-list)
-(global-set-key (kbd "<f9>") 'bookmark-set)
+(global-set-key (kbd "<f8>") 'bookmark-set)
+(global-set-key (kbd "<f9>") 'bookmark-bmenu-list)
 
 (provide 'init-bookmarks)
