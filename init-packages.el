e799cbc6ac6aa108134a92680003007aa4e32a51
Ronert Obst <ronert.obst@gmail.com>
2013-11-26 11:22:11 +0100

    added projectile to guide-key

--- init-packages.el
+++ init-packages.el
@@ -80,7 +80,7 @@
 
 ;; guide-key
 (require-package 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x v" "C-c"))
+(setq guide-key/guide-key-sequence '("C-x r" "C-x v" "C-c p"))
 (guide-key-mode 1)
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
