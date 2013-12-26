e75223cd8fc75c955698b28ac771add7cda3110e
Ronert Obst <ronert.obst@gmail.com>
2013-12-26 11:30:19 +0100

    renamed some functions

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,39 +1,35 @@
 (require-package 'yasnippet)
 (setq yas-snippet-dirs
-      '("~/Dropbox/dotfiles/.emacs.d/yasnippet-snippets"            ;; yasnippet snippets
-        "~/Dropbox/dotfiles/.emacs.d/snippets" ;; personal snippets
+      '("~/Dropbox/dotfiles/.emacs.d/snippets"
         ))
 
 (yas-global-mode 1)
+
 (global-set-key (kbd "C--") 'yas-expand)
 
-(setq-default yas-prompt-functions '(yas-ido-prompt yas-dropdown-prompt))
-
 ;; Jump to end of snippet definition
-(define-key yas-keymap (kbd "<return>") 'yas/exit-all-snippets)
+(define-key yas-keymap (kbd "<return>") 'yas-exit-all-snippets)
 
 ;; Inter-field navigation
-(defun yas/goto-end-of-active-field ()
+(defun yas-goto-end-of-active-field ()
   (interactive)
   (let* ((snippet (car (yas--snippets-at-point)))
         (position (yas--field-end (yas--snippet-active-field snippet))))
     (if (= (point) position)
         (move-end-of-line 1)
       (goto-char position))))
-
-(defun yas/goto-start-of-active-field ()
+(defun yas-goto-start-of-active-field ()
   (interactive)
   (let* ((snippet (car (yas--snippets-at-point)))
         (position (yas--field-start (yas--snippet-active-field snippet))))
     (if (= (point) position)
         (move-beginning-of-line 1)
       (goto-char position))))
-
-(define-key yas-keymap (kbd "C-e") 'yas/goto-end-of-active-field)
-(define-key yas-keymap (kbd "C-a") 'yas/goto-start-of-active-field)
+(define-key yas-keymap (kbd "C-e") 'yas-goto-end-of-active-field)
+(define-key yas-keymap (kbd "C-a") 'yas-goto-start-of-active-field)
 
 ;; No dropdowns please, yas
-(setq yas-prompt-functions '(yas/ido-prompt yas/completing-prompt))
+(setq-default yas-prompt-functions '(yas-ido-prompt yas-dropdown-prompt))
 
 ;; No need to be so verbose
 (setq yas-verbosity 1)
