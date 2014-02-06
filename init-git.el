472588ca52fdc0b0a20ff95ae6053555276a4384
Ronert Obst <ronert.obst@gmail.com>
2014-02-06 22:31:28 +0100

    temp disable fullframe

--- init-git.el
+++ init-git.el
@@ -35,7 +35,8 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (define-key global-map "\M-\C-g" 'magit-status)
 
-(require-package 'fullframe)
-(fullframe magit-status magit-mode-quit-window :magit-fullscreen nil)
+;; (require-package 'fullframe)
+;; (after-load 'magit
+;; (fullframe magit-status magit-mode-quit-window))
 
 (provide 'init-git)
