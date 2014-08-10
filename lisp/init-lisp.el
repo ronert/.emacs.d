1f7d5d720db6f0526b41549b1f543f7362146485
Ronert Obst <ronert.obst@gmail.com>
2014-08-10 22:19:04 +0200

    latest purcell changes

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -346,15 +346,15 @@ Kills existing SLIME session, if any."
   'paredit-wrap-round-from-behind)
 
 ;; C-x C-e eval region if region is active
-(defun sanityinc/eval-last-sexp-or-region (beg end prefix)
+(defun sanityinc/eval-last-sexp-or-region (prefix)
   "Eval region from BEG to END if active, otherwise the last sexp."
-  (interactive "r\nP")
-  (if (use-region-p)
-      (eval-region beg end)
+  (interactive "P")
+  (if (and (mark) (use-region-p))
+      (eval-region (min (point) (mark)) (max (point) (mark)))
     (pp-eval-last-sexp prefix)))
+
 (after-load 'lisp-mode
   (define-key emacs-lisp-mode-map (kbd "C-x C-e") 'sanityinc/eval-last-sexp-or-region)
   (define-key emacs-lisp-mode-map (kbd "C-x C-a") 'pp-macroexpand-last-sexp))
-
 
 (provide 'init-lisp)
