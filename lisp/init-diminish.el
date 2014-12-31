8a2be43f6304bbe7e45b04c39f716d9cb44b070c
Ronert Obst <ronert.obst@gmail.com>
2014-12-31 18:09:15 +0100

    use-package

--- lisp/init-diminish.el
+++ lisp/init-diminish.el
@@ -1,13 +1,15 @@
-(require-package 'diminish)
-(diminish 'anzu-mode)
-(diminish 'guide-key-mode)
-(diminish 'google-this-mode)
-(diminish 'projectile-mode)
-(diminish 'eldoc-mode)
-(diminish 'auto-complete-mode)
-(diminish 'whole-line-or-region-mode)
-(diminish 'yas-minor-mode)
-(diminish 'abbrev-mode)
-;; (diminish 'global-visual-line-mode)
+(use-package diminish
+  :ensure t
+  :pin melpa-stable
+  :init
+  (diminish 'anzu-mode)
+  (diminish 'guide-key-mode)
+  (diminish 'google-this-mode)
+  (diminish 'projectile-mode)
+  (diminish 'eldoc-mode)
+  (diminish 'auto-complete-mode)
+  (diminish 'whole-line-or-region-mode)
+  (diminish 'yas-minor-mode)
+  (diminish 'abbrev-mode))
 
 (provide 'init-diminish)
