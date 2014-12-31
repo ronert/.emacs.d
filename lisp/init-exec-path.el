25c7337579aa546a39a0cc72795df570d37fd489
Ronert Obst <ronert.obst@gmail.com>
2014-12-31 18:40:16 +0100

    use-package

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,11 +1,15 @@
-(require-package 'exec-path-from-shell)
+(use-package exec-path-from-shell
+  :ensure t
+  :pin melpa-stable
+  :init
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
+  :config
+  (progn
+    (setenv "PATH" (concat "/usr/texbin:/usr/local/bin:" (getenv "PATH")))
+    (setenv "PATH" (concat "~/anaconda/bin:" (getenv "PATH")))
 
-(setenv "PATH" (concat "/usr/texbin:/usr/local/bin:" (getenv "PATH")))
-(setenv "PATH" (concat "~/anaconda/bin:" (getenv "PATH")))
-
-(setq-default ispell-program-name "/usr/local/bin/aspell")
-
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+    (setq-default ispell-program-name "/usr/local/bin/aspell"))
+  )
 
 (provide 'init-exec-path)
