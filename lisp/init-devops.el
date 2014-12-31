5219cd99ee75947cbdb0fa237e681ec8cc292673
Ronert Obst <ronert.obst@gmail.com>
2014-12-31 18:06:35 +0100

    use-package

--- lisp/init-devops.el
+++ lisp/init-devops.el
@@ -1,5 +1,11 @@
-(require-package 'puppet-mode)
-(require-package 'vagrant)
-(require-package 'dockerfile-mode)
+(use-package puppet-mode
+  :ensure t
+  :pin melpa-stable)
+(use-package vagrant
+  :ensure t
+  :pin melpa-stable)
+(use-package dockerfile-mode
+  :ensure t
+  :pin melpa-stable)
 
 (provide 'init-devops)
