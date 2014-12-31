11b7e258af25caaa66432cb768827a16d18aeddd
Ronert Obst <ronert.obst@gmail.com>
2014-12-31 16:56:03 +0100

    changed yasnippet to use use-package

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,44 +1,46 @@
-(require-package 'yasnippet)
-(setq yas-snippet-dirs
-      '("/Users/ronert/Dropbox/dotfiles/.emacs.d/snippets"
-        "/Users/ronert/Dropbox/dotfiles/.emacs.d/snippets/shnippet"))
+(use-package yasnippet
+             :ensure t
+             :pin melpa-stable
+             :bind ("C--" . yas-expand)
+             :init
+             (yas-global-mode 1)
+             :config
+             (progn
+               (setq yas-snippet-dirs
+                     '("/Users/ronert/Dropbox/dotfiles/.emacs.d/snippets"
+                       "/Users/ronert/Dropbox/dotfiles/.emacs.d/snippets/shnippet"))
+               (define-key yas-keymap (kbd "<return>") 'yas-exit-all-snippets)
 
-(yas-global-mode 1)
+               ;; Inter-field navigation
+               (defun yas-goto-end-of-active-field ()
+                 (interactive)
+                 (let* ((snippet (car (yas--snippets-at-point)))
+                        (position (yas--field-end (yas--snippet-active-field snippet))))
+                   (if (= (point) position)
+                       (move-end-of-line 1)
+                     (goto-char position))))
+               (defun yas-goto-start-of-active-field ()
+                 (interactive)
+                 (let* ((snippet (car (yas--snippets-at-point)))
+                        (position (yas--field-start (yas--snippet-active-field snippet))))
+                   (if (= (point) position)
+                       (move-beginning-of-line 1)
+                     (goto-char position))))
+               (define-key yas-keymap (kbd "C-e") 'yas-goto-end-of-active-field)
+               (define-key yas-keymap (kbd "C-a") 'yas-goto-start-of-active-field)
 
-(global-set-key (kbd "C--") 'yas-expand)
+               ;; No dropdowns please, yas
+               (setq-default yas-prompt-functions '(yas-ido-prompt yas-dropdown-prompt))
 
-;; Jump to end of snippet definition
-(define-key yas-keymap (kbd "<return>") 'yas-exit-all-snippets)
+               ;; No need to be so verbose
+               (setq yas-verbosity 1)
 
-;; Inter-field navigation
-(defun yas-goto-end-of-active-field ()
-  (interactive)
-  (let* ((snippet (car (yas--snippets-at-point)))
-        (position (yas--field-end (yas--snippet-active-field snippet))))
-    (if (= (point) position)
-        (move-end-of-line 1)
-      (goto-char position))))
-(defun yas-goto-start-of-active-field ()
-  (interactive)
-  (let* ((snippet (car (yas--snippets-at-point)))
-        (position (yas--field-start (yas--snippet-active-field snippet))))
-    (if (= (point) position)
-        (move-beginning-of-line 1)
-      (goto-char position))))
-(define-key yas-keymap (kbd "C-e") 'yas-goto-end-of-active-field)
-(define-key yas-keymap (kbd "C-a") 'yas-goto-start-of-active-field)
+               ;; Wrap around region
+               (setq yas-wrap-around-region t)
 
-;; No dropdowns please, yas
-(setq-default yas-prompt-functions '(yas-ido-prompt yas-dropdown-prompt))
-
-;; No need to be so verbose
-(setq yas-verbosity 1)
-
-;; Wrap around region
-(setq yas-wrap-around-region t)
-
-;; Unbind yas from tab
-(define-key yas-minor-mode-map [(tab)] nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
+               ;; Unbind yas from tab
+               (define-key yas-minor-mode-map [(tab)] nil)
+               (define-key yas-minor-mode-map (kbd "TAB") nil)
+               ))
 
 (provide 'init-yasnippet)
