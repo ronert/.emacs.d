a905d65e43520ee5145a0d2296e9d1a08e58a495
Ronert Obst <ronert.obst@gmail.com>
2014-12-31 17:36:36 +0100

    use-package

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,31 +1,42 @@
-(require-package 'company)
-(add-hook 'after-init-hook 'global-company-mode)
+(use-package company
+  :ensure t
+  :pin melpa-stable
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :config (progn
+            ;; latex completion
+            (use-package company-auctex
+              :ensure t
+              :config
+              (progn
+                (company-auctex-init)
+                (require-package 'company-math)
+                (add-to-list 'company-backend 'company-math-symbols-unicode)
+                (add-to-list 'company-backend 'company-math-symbols-latex)
+                (add-to-list 'company-backend 'company-latex-commands)))
 
-;; latex completion
-(require-package 'company-auctex)
-(company-auctex-init)
-(require-package 'company-math)
-(add-to-list 'company-backend 'company-math-symbols-unicode)
-(add-to-list 'company-backend 'company-math-symbols-latex)
-(add-to-list 'company-backend 'company-latex-commands)
+            ;; haskell completion
+            (use-package company-ghc
+              :ensure t
+              :pin melpa-stable
+              :config (add-to-list 'company-backends 'company-ghc))
 
-;; haskell completion
-(require-package 'company-ghc)
-(add-to-list 'company-backends 'company-ghc)
+            ;; python completion
+            (use-package company-anaconda
+              :ensure t
+              :config (add-to-list 'company-backends 'company-anaconda))
 
-;; python completion
-(require-package 'company-anaconda)
-(add-to-list 'company-backends 'company-anaconda)
+            ;; sql completion
+            (use-package company-edbi
+              :ensure t
+              :config (add-to-list 'company-backends 'company-edbi))
 
-;; sql completion
-(require-package 'company-edbi)
-(add-to-list 'company-backends 'company-edbi)
-
-;; r completion
-(require 'company-ess)
-(add-to-list 'company-backends 'company-ess-backend)
-
-;; Set a global completion hotkey
-(global-set-key (kbd "C-.") 'company-complete)
+            ;; r completion
+            (use-package company-ess
+              :ensure t
+              :config (add-to-list 'company-backends 'company-ess-backend))
+            )
+  ;; Set a global completion hotkey
+  :bind ("C-." . company-complete)
+  )
 
 (provide 'init-company)
