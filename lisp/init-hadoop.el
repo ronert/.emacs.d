7710c1aedd99c636f7899ee5f2060f3a2fcc7efc
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 15:29:24 +0100

    use-package

--- lisp/init-hadoop.el
+++ lisp/init-hadoop.el
@@ -1,14 +1,18 @@
-(require-package 'pig-mode)
-(require-package 'hive)
+(use-package pig-mode
+  :ensure t
+  :config
+  (progn
+    (autoload 'pig-snippets-initialize "pig-snippets" nil nil nil)
+    (eval-after-load 'yasnippet '(pig-snippets-initialize))))
+
+(use-package hive
+  :ensure t)
 
 ;; (setq pig-executable "/path/to/pig-0.11.1/bin/pig")
 ;; (setq pig-executable-options '("-x" "local"))
 ;; (setq pig-executable-prompt-regexp "^grunt> ")
 ;; (setq pig-indent-level 4)
 ;; (setq pig-version "0.11.1")
-
-(autoload 'pig-snippets-initialize "pig-snippets" nil nil nil)
-(eval-after-load 'yasnippet '(pig-snippets-initialize))
 
 (provide 'init-hadoop)
 ;;; hadoop.el ends here
