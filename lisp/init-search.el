826a8fc08c9dea38e83f9a45039a6016b7bc866c
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 17:34:10 +0100

    use package

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -1,27 +1,40 @@
 ;; Google this
-(require-package 'google-this)
-(google-this-mode 1)
-(global-set-key (kbd "C-c /") 'google-this-mode-submap)
+(use-package google-this
+  :ensure t
+  :pin melpa-stable
+  :init (google-this-mode 1)
+  :bind (kbd "C-c /" . google-this-mode-submap))
 
 ;; Silver searcher
-(require-package 'ag)
-(setq ag-highlight-search t)
-(global-set-key (kbd "C-c 5") 'ag-regexp)
-(global-set-key (kbd "C-c 6") 'ag-project)
+(use-package ag
+  :ensure t
+  :pin melpa-stable
+  :config (setq ag-highlight-search t)
+  :bind ((global-set-key (kbd "C-c 5") 'ag-regexp)
+         (global-set-key (kbd "C-c 6") 'ag-project)
+         (global-set-key (kbd "C-c 7") 'ag-project-at-point)
+         (global-set-key (kbd "C-c 8") 'ag-regexp-project-at-point)
+         ))
 
-(global-set-key (kbd "C-c 7") 'ag-project-at-point)
-(global-set-key (kbd "C-c 8") 'ag-regexp-project-at-point)
+;; Ack
+(use-package ack-and-a-half
+  :ensure t
+  :pin melpa-stable
+  :bind
+  ((global-set-key (kbd "C-c 1") 'ack-and-a-half)
+   (global-set-key (kbd "C-c 2") 'ack-and-a-half-same)
+   (global-set-key (kbd "C-c 3") 'ack-and-a-half-find-file)
+   (global-set-key (kbd "C-c 4") 'ack-and-a-half-find-file-same)))
 
-(global-set-key (kbd "C-c 1") 'ack-and-a-half)
-(global-set-key (kbd "C-c 2") 'ack-and-a-half-same)
-(global-set-key (kbd "C-c 3") 'ack-and-a-half-find-file)
-(global-set-key (kbd "C-c 4") 'ack-and-a-half-find-file-same)
-
+;; rgrep
 (define-key global-map "\C-x\C-r" 'rgrep)
 
 ;; anzu
 ;; Show number of matches while searching
-(require-package 'anzu)
-(global-anzu-mode t)
+(use-package anzu
+  :ensure t
+  :pin melpa-stable
+  :init
+  (global-anzu-mode t))
 
 (provide 'init-search)
