195d356ab323a6b5cb6bbcb41fc4eb9c818361d2
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 15:24:21 +0100

    use-package

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,48 +1,66 @@
-(require 'magit)
+(use-package magit
+  :ensure t
+  :pin melpa-stable
+  :config
+  (progn
+    (defun magit-toggle-whitespace ()
+      (interactive)
+      (if (member "-w" magit-diff-options)
+          (magit-dont-ignore-whitespace)
+        (magit-ignore-whitespace)))
+
+    (defun magit-ignore-whitespace ()
+      (interactive)
+      (add-to-list 'magit-diff-options "-w")
+      (magit-refresh))
+
+    (defun magit-dont-ignore-whitespace ()
+      (interactive)
+      (setq magit-diff-options (remove "-w" magit-diff-options))
+      (magit-refresh))
+
+    (use-package fullframe
+      :ensure t
+      :init (after-load 'magit
+              (fullframe magit-status magit-mode-quit-window)))
+
+    (global-set-key [f16] 'magit-status)
+    (global-set-key [f17] 'magit-init)
+    (global-set-key (kbd "C-x g") 'magit-status)
+    (define-key global-map "\M-\C-g" 'magit-status)
+    )
+  )
 
 ;; Git Messenger
-(require-package 'git-messenger)
-(global-set-key (kbd "C-x v p") 'git-messenger:popup-message)
+(use-package git-messenger
+  :ensure t
+  :pin melpa-stable
+  :bind
+  ("C-x v p" . git-messenger:popup-message))
 
-(global-set-key (kbd "C-=") 'git-gutter:toggle)
-(global-set-key (kbd "C-M-=") 'git-gutter:popup-diff)
+(use-package git-gutter
+  :ensure t
+  :bind
+  (
+   ("C-=" . git-gutter:toggle)
+   ("C-M-=" . git-gutter:popup-diff))
+  )
 
 
-(defun magit-toggle-whitespace ()
-  (interactive)
-  (if (member "-w" magit-diff-options)
-      (magit-dont-ignore-whitespace)
-    (magit-ignore-whitespace)))
+(use-package gist
+  :ensure t
+  :pin melpa-stable
+  :config
+  (setq gist-view-gist t))
 
-(defun magit-ignore-whitespace ()
-  (interactive)
-  (add-to-list 'magit-diff-options "-w")
-  (magit-refresh))
 
-(defun magit-dont-ignore-whitespace ()
-  (interactive)
-  (setq magit-diff-options (remove "-w" magit-diff-options))
-  (magit-refresh))
+(use-package diff-hl
+  :ensure t
+  :init
+  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
 
-(global-set-key [f16] 'magit-status)
-(global-set-key [f17] 'magit-init)
-
-;; (require-package 'magithub)
-
-(require-package 'gist)
-(setq gist-view-gist t)
-
-(global-set-key (kbd "C-x g") 'magit-status)
-(define-key global-map "\M-\C-g" 'magit-status)
-
-(require-package 'fullframe)
-(after-load 'magit
-  (fullframe magit-status magit-mode-quit-window))
-
-(require-package 'diff-hl)
-(add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
-
-;; git-timemachine
-(require-package 'git-timemachine)
+(use-package git-timemachine
+  :ensure t
+  :pin melpa-stable)
 
 (provide 'init-git)
