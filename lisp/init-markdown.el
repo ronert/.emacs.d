08b2b7d26e8dee02d5bea5f6ee1a4f8721d6b31b
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 16:46:19 +0100

    use-package

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,13 +1,18 @@
 ;; Markdown
-(require-package 'markdown-mode)
+(use-package markdown-mode
+  :ensure t
+  :pin melpa-stable
+  :init
+  (progn
+    (autoload 'markdown-mode "markdown-mode.el"
+      "Major mode for editing Markdown files" t)
+    (setq auto-mode-alist
+          (cons '("\\.markdown$" . markdown-mode)
+                (cons '("\\.md$" . markdown-mode) auto-mode-alist)))
 
-(autoload 'markdown-mode "markdown-mode.el"
-  "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.markdown$" . markdown-mode)
-            (cons '("\\.md$" . markdown-mode) auto-mode-alist)))
+    ))
 
-(provide 'markdown)
+
 ;;; markdown.el ends here
 
 (provide 'init-markdown)
