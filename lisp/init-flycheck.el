ce3085b1ec25ea377d6464ebb5acce7d314f05e0
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 01:18:41 +0100

    use-package

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,12 +1,15 @@
-(require-package 'flycheck)
-(add-hook 'after-init-hook 'global-flycheck-mode)
-
-;; Override default flycheck triggers
-(setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
-flycheck-idle-change-delay 0.8)
-
-(require-package 'flycheck-tip)
-(global-set-key (kbd "C-c C-n") 'flycheck-tip-cycle)
-(setq flycheck-tip-avoid-show-func nil)
-
+(use-package flycheck
+  :ensure t
+  :pin melpa-stable
+  :init (add-hook 'after-init-hook 'global-flycheck-mode)
+  :config
+  (progn
+    ;; Override default flycheck triggers
+    (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+          flycheck-idle-change-delay 0.8)
+    (use-package flycheck-tip
+      :ensure t
+      :bind ("C-c C-n" . flycheck-tip-cycle)
+      :config (setq flycheck-tip-avoid-show-func nil))
+    ))
 (provide 'init-flycheck)
