95eff34b53d8b8c29ddbf40b5369245a585a52ae
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 17:28:18 +0100

    use-package

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,29 +1,44 @@
-(require 'python)
+(use-package python
+  :init
+  (progn
+    (add-hook 'python-mode-hook 'run-coding-hook)
+    (use-package anaconda-mode
+      :ensure t
+      :init (add-hook 'python-mode-hook 'anaconda-mode))
+    (add-hook 'python-mode-hook 'my-python-customizations)
+    (defun my-python-customizations ()
+      "set up my personal customizations for python mode"
+      ;; put other customizations in here
+      (define-key python-mode-map (kbd "C-c d") 'add-docstring))
+    (defun add-docstring (&optional arg)
+      "Keyboard macro."
+      (interactive "p")
+      (kmacro-exec-ring-item `(,(kbd "C-r def C-n C-a C-m C-p C-i C-u 6 \" C-u 3 C-b") 0 "%d")
+                             arg)
+      )
+    ))
 
 ;; elpy
-(require 'elpy)
-(elpy-enable)
-(elpy-use-ipython)
+(use-package elpy
+  :ensure t
+  :pin elpy
+  :init
+  (progn
+    (elpy-enable)
+    (elpy-use-ipython)
+    ))
 
 ;; ein
-(require-package 'websocket)
-(require-package 'ein)
-(setq ein:connect-default-notebook "8888/ipythonNotebook")
-(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
-
-;; python mode
-(add-hook 'python-mode-hook 'my-python-customizations)
-(defun my-python-customizations ()
-  "set up my personal customizations for python mode"
-  ;; put other customizations in here
-  (define-key python-mode-map (kbd "C-c d") 'add-docstring))
-(defun add-docstring (&optional arg)
-  "Keyboard macro."
-  (interactive "p")
-  (kmacro-exec-ring-item `(,(kbd "C-r def C-n C-a C-m C-p C-i C-u 6 \" C-u 3 C-b") 0 "%d")
-                         arg))
-
-(add-hook 'python-mode-hook 'run-coding-hook)
-(add-hook 'python-mode-hook 'anaconda-mode)
+(use-package websocket
+  :ensure t)
+(use-package ein
+  :ensure t
+  :pin melpa-stable
+  :config
+  (progn
+    (setq ein:connect-default-notebook "8888/ipythonNotebook")
+    (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+    )
+  )
 
 (provide 'init-python)
