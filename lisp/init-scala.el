0593e721a5173295028d1758f4ccbc55c2902bb1
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 17:30:39 +0100

    use-package

--- lisp/init-scala.el
+++ lisp/init-scala.el
@@ -1,23 +1,33 @@
 ;; scala-mode2
-(require-package 'scala-mode2)
+(use-package scala-mode2
+  :ensure t
+  :pin melpa-stable
+  :config
+  (progn
+    (add-hook 'scala-mode-hook 'run-coding-hook)
+    (setq scala-indent:align-forms t)
+    (setq scala-indent:align-parameters t)
 
-(add-hook 'scala-mode-hook 'run-coding-hook)
+    ;; ensime
+    (use-package ensime
+      :ensure t
+      :init (add-hook 'scala-mode-hook 'ensime-scala-mode-hook))
 
-(setq scala-indent:align-forms t)
-(setq scala-indent:align-parameters t)
+    ;; sbt-mode
+    (use-package sbt-mode
+      :ensure t
+      :init
+      (progn
+        (add-hook 'scala-mode-hook '(lambda ()
+                                      ;; sbt-find-definitions is a command that tries to find (with grep)
+                                      ;; the definition of the thing at point.
+                                      (local-set-key (kbd "M-.") 'sbt-find-definitions)
 
-;; ensime
-(require-package 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+                                      ;; use sbt-run-previous-command to re-compile your code after changes
+                                      (local-set-key (kbd "C-x '") 'sbt-run-previous-command)
+                                      ))))
+    )
+  )
 
-;; sbt-mode
-(require-package 'sbt-mode)
-(add-hook 'scala-mode-hook '(lambda ()
-                              ;; sbt-find-definitions is a command that tries to find (with grep)
-                              ;; the definition of the thing at point.
-                              (local-set-key (kbd "M-.") 'sbt-find-definitions)
 
-                              ;; use sbt-run-previous-command to re-compile your code after changes
-                              (local-set-key (kbd "C-x '") 'sbt-run-previous-command)
-                              ))
 (provide 'init-scala)
