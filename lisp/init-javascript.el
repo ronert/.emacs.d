9369f26c6d5982123ab435771696dd0fcd15a190
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 16:38:38 +0100

    use-package

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,64 +1,76 @@
-(require-package 'json-mode)
+(use-package json-mode
+  :ensure t
+  :pin melpa-stable)
 (when (>= emacs-major-version 24)
-  (require-package 'js2-mode)
-  (require-package 'ac-js2)
-  (require-package 'coffee-mode))
-(require-package 'js-comint)
+  (use-package js2-mode
+    :ensure t
+    :pin melpa-stable
+    :init
+    (progn
+      (defcustom preferred-javascript-mode
+        (first (remove-if-not #'fboundp '(js2-mode js-mode)))
+        "Javascript mode to use for .js files."
+        :type 'symbol
+        :group 'programming
+        :options '(js2-mode js-mode))
+      (defvar preferred-javascript-indent-level 2)
+      ;; Need to first remove from list if present, since elpa adds entries too, which
+      ;; may be in an arbitrary order
+      (eval-when-compile (require 'cl))
+      (setq auto-mode-alist (cons `("\\.js\\(\\.erb\\)?\\'" . ,preferred-javascript-mode)
+                                  (loop for entry in auto-mode-alist
+                                        unless (eq preferred-javascript-mode (cdr entry))
+                                        collect entry)))
+      )
+    :config
+    (progn
+      ;; js2-mode
+      (after-load 'js2-mode
+        ;; Disable js2 mode's syntax error highlighting by default...
+        (setq-default js2-mode-show-parse-errors nil
+                      js2-mode-show-strict-warnings nil)
+        ;; ... but enable it if flycheck can't handle javascript
+        (autoload 'flycheck-get-checker-for-buffer "flycheck")
+        (defun sanityinc/disable-js2-checks-if-flycheck-active ()
+          (unless (flycheck-get-checker-for-buffer)
+            (set (make-local-variable 'js2-mode-show-parse-errors) t)
+            (set (make-local-variable 'js2-mode-show-strict-warnings) t)))
+        (add-hook 'js2-mode-hook 'sanityinc/disable-js2-checks-if-flycheck-active)
 
-(defcustom preferred-javascript-mode
-  (first (remove-if-not #'fboundp '(js2-mode js-mode)))
-  "Javascript mode to use for .js files."
-  :type 'symbol
-  :group 'programming
-  :options '(js2-mode js-mode))
-(defvar preferred-javascript-indent-level 2)
+        (add-hook 'js2-mode-hook (lambda () (setq mode-name "JS2")))
 
-;; Need to first remove from list if present, since elpa adds entries too, which
-;; may be in an arbitrary order
-(eval-when-compile (require 'cl))
-(setq auto-mode-alist (cons `("\\.js\\(\\.erb\\)?\\'" . ,preferred-javascript-mode)
-                            (loop for entry in auto-mode-alist
-                                  unless (eq preferred-javascript-mode (cdr entry))
-                                  collect entry)))
+        (setq-default
+         js2-basic-offset preferred-javascript-indent-level
+         js2-bounce-indent-p nil)
 
+        (after-load 'js2-mode
+          (js2-imenu-extras-setup)))
 
-;; js2-mode
-(after-load 'js2-mode
-  ;; Disable js2 mode's syntax error highlighting by default...
-  (setq-default js2-mode-show-parse-errors nil
-                js2-mode-show-strict-warnings nil)
-  ;; ... but enable it if flycheck can't handle javascript
-  (autoload 'flycheck-get-checker-for-buffer "flycheck")
-  (defun sanityinc/disable-js2-checks-if-flycheck-active ()
-    (unless (flycheck-get-checker-for-buffer)
-      (set (make-local-variable 'js2-mode-show-parse-errors) t)
-      (set (make-local-variable 'js2-mode-show-strict-warnings) t)))
-  (add-hook 'js2-mode-hook 'sanityinc/disable-js2-checks-if-flycheck-active)
+      ;; js-mode
+      (setq-default js-indent-level preferred-javascript-indent-level)
 
-  (add-hook 'js2-mode-hook (lambda () (setq mode-name "JS2")))
-
-  (setq-default
-   js2-basic-offset preferred-javascript-indent-level
-   js2-bounce-indent-p nil)
-
-  (after-load 'js2-mode
-    (js2-imenu-extras-setup)))
-
-;; js-mode
-(setq-default js-indent-level preferred-javascript-indent-level)
+      (add-to-list 'interpreter-mode-alist (cons "node" preferred-javascript-mode))))
+  ;; (use-package ac-js2) using company
+  (use-package coffee-mode
+    :ensure t
+    :pin melpa-stable))
+(use-package js-comint
+  :ensure t)
 
 
-(add-to-list 'interpreter-mode-alist (cons "node" preferred-javascript-mode))
 
-
+
+
+
 ;; Javascript nests {} and () a lot, so I find this helpful
+(use-package rainbow-delimiters
+  :ensure t
+  :pin melpa-stable
+  :init
+  (dolist (hook '(js2-mode-hook js-mode-hook json-mode-hook))
+    (add-hook hook 'rainbow-delimiters-mode)))
 
-(require-package 'rainbow-delimiters)
-(dolist (hook '(js2-mode-hook js-mode-hook json-mode-hook))
-  (add-hook hook 'rainbow-delimiters-mode))
 
-
-
 ;;; Coffeescript
 
 (after-load 'coffee-mode
@@ -93,10 +105,14 @@
 ;; ---------------------------------------------------------------------------
 
 (when (and (>= emacs-major-version 24) (featurep 'js2-mode))
-  (require-package 'skewer-mode)
-  (after-load 'skewer-mode
-    (add-hook 'skewer-mode-hook
-              (lambda () (inferior-js-keys-mode -1)))))
+  (use-package skewer-mode
+    :ensure t
+    :pin melpa-stable
+    :init
+    (after-load 'skewer-mode
+      (add-hook 'skewer-mode-hook
+                (lambda () (inferior-js-keys-mode -1)))))
+  )
 
 
 (provide 'init-javascript)
