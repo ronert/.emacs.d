cc6cdb60febd1511cf7789e04e5b57ff9712d11c
Ronert Obst <ronert.obst@gmail.com>
2015-01-01 17:45:48 +0100

    use-package

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -1,64 +1,82 @@
 ;; save a list of open files in ~/.emacs.d/.emacs.desktop
 ;; save the desktop file automatically if it already exists
-(require-package 'desktop)
-(desktop-save-mode 1)
-(defun my-desktop-save ()
-  (interactive)
-  ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
-  (if (eq (desktop-owner) (emacs-pid))
-      (desktop-save desktop-dirname)))
-(add-hook 'auto-save-hook 'my-desktop-save)
+(use-package desktop
+  :ensure t
+  :init (progn
+          (desktop-save-mode 1)
+          (defun my-desktop-save ()
+            (interactive)
+            ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+            (if (eq (desktop-owner) (emacs-pid))
+                (desktop-save desktop-dirname)))
+          (add-hook 'auto-save-hook 'my-desktop-save)
 
-(run-at-time 3600 3600 'my-desktop-save)
+          (run-at-time 3600 3600 'my-desktop-save)
 
-(setq desktop-path '("~/"))
-(setq desktop-save 'if-exists)
-(desktop-save-mode 1)
-(defadvice desktop-read (around trace-desktop-errors)
-  (let ((debug-on-error t))
-    ad-do-it))
-(desktop-save-mode 1)
+          (setq desktop-path '("~/"))
+          (setq desktop-save 'if-exists)
+          (desktop-save-mode 1)
+          (defadvice desktop-read (around trace-desktop-errors)
+            (let ((debug-on-error t))
+              ad-do-it))
+          (desktop-save-mode 1)
+          )
+  )
+
 
 ;;----------------------------------------------------------------------------;; Restore histories and registers after saving
 ;;----------------------------------------------------------------------------
-(require-package 'session)
+(use-package session
+  :ensure t
+  :pin melpa-stable
+  :init
+  (progn
+    (setq session-save-file (expand-file-name "~/.session"))
+    (add-hook 'after-init-hook 'session-initialize)
 
-(setq session-save-file (expand-file-name "~/.session"))
-(add-hook 'after-init-hook 'session-initialize)
+    ;; save a bunch of variables to the desktop file
+    ;; for lists specify the len of the maximal saved data also
+    (setq desktop-globals-to-save
+          (append '((extended-command-history . 30)
+                    (file-name-history        . 100)
+                    (ido-last-directory-list  . 100)
+                    (ido-work-directory-list  . 100)
+                    (ido-work-file-list       . 100)
+                    (grep-history             . 30)
+                    (compile-history          . 30)
+                    (minibuffer-history       . 50)
+                    (query-replace-history    . 60)
+                    (read-expression-history  . 60)
+                    (regexp-history           . 60)
+                    (regexp-search-ring       . 20)
+                    (search-ring              . 20)
+                    (comint-input-ring        . 50)
+                    (shell-command-history    . 50)
+                    desktop-missing-file-warning
+                    tags-file-name
+                    register-alist)))
+    ))
 
-;; save a bunch of variables to the desktop file
-;; for lists specify the len of the maximal saved data also
-(setq desktop-globals-to-save
-      (append '((extended-command-history . 30)
-                (file-name-history        . 100)
-                (ido-last-directory-list  . 100)
-                (ido-work-directory-list  . 100)
-                (ido-work-file-list       . 100)
-                (grep-history             . 30)
-                (compile-history          . 30)
-                (minibuffer-history       . 50)
-                (query-replace-history    . 60)
-                (read-expression-history  . 60)
-                (regexp-history           . 60)
-                (regexp-search-ring       . 20)
-                (search-ring              . 20)
-                (comint-input-ring        . 50)
-                (shell-command-history    . 50)
-                desktop-missing-file-warning
-                tags-file-name
-                register-alist)))
 
 ;; Save point position between sessions
-(require-package 'saveplace)
-(setq-default save-place t)
-(setq save-place-file (expand-file-name "~/.places"))
+(use-package saveplace
+  :ensure t
+  :init
+  (setq-default save-place t)
+  :config
+  (setq save-place-file (expand-file-name "~/.places"))
+  )
 
 ;; Workgroups
-(require 'workgroups)
-(global-unset-key (kbd "C-y"))
-(setq wg-prefix-key (kbd "C-y"))
-(workgroups-mode 1)
-(wg-load "~/workgroups")
-(run-at-time 3600 3600 'wg-update-all-workgroups-and-save)
+(use-package workgroups
+  :ensure t
+  :init (progn
+          (global-unset-key (kbd "C-y"))
+          (setq wg-prefix-key (kbd "C-y"))
+          (workgroups-mode 1)
+          (wg-load "~/workgroups")
+          (run-at-time 3600 3600 'wg-update-all-workgroups-and-save)
+          )
+  )
 
 (provide 'init-sessions)
