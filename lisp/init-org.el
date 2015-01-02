cc0b1ffd5200f12ffa6225913ff2e079b5b7900b
Ronert Obst <ronert.obst@gmail.com>
2015-01-02 12:56:43 +0100

    don’t need yas safe expand anymore

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -25,14 +25,14 @@
                          '(apply ,original-command))
                    (local-set-key [tab] 'yas-expand))))
 
-    (defun yas/org-very-safe-expand ()
-      (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
-    (add-hook 'org-mode-hook
-              (lambda ()
-                (make-variable-buffer-local 'yas/trigger-key)
-                (setq yas/trigger-key [tab])
-                (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
-                (define-key yas/keymap [tab] 'yas/next-field)))
+    ;; (defun yas/org-very-safe-expand ()
+    ;;   (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+    ;; (add-hook 'org-mode-hook
+    ;;           (lambda ()
+    ;;             (make-variable-buffer-local 'yas/trigger-key)
+    ;;             (setq yas/trigger-key [tab])
+    ;;             (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+    ;;             (define-key yas/keymap [tab] 'yas/next-field)))
 
 
     (add-hook 'org-mode-hook
