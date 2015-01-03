b4d978b1cb63bcacd2b86918320a2220dcd58c82
Ronert Obst <ronert.obst@gmail.com>
2015-01-03 17:42:35 +0100

    removed ack

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -17,23 +17,11 @@
             (use-package wgrep-ag
               :ensure t)
             )
-  :bind (("C-c 5" . ag-regexp)
-         ("C-c 6" . ag-project)
-         ("C-c 7" . ag-project-at-point)
-         ("C-c 8" . ag-regexp-project-at-point)
+  :bind (("C-c 1" . ag-regexp)
+         ("C-c 2" . ag-project)
+         ("C-c 3" . ag-project-at-point)
+         ("C-c 4" . ag-regexp-project-at-point)
          ))
-
-;; Ack
-(use-package ack-and-a-half
-  :ensure t
-  :pin melpa-stable
-  :config (use-package wgrep-ack
-            :ensure t)
-  :bind
-  (("C-c 1" . ack-and-a-half)
-   ("C-c 2" . ack-and-a-half-same)
-   ("C-c 3" . ack-and-a-half-find-file)
-   ("C-c 4" . ack-and-a-half-find-file-same)))
 
 ;; rgrep
 (define-key global-map "\C-x\C-r" 'rgrep)
