da6e217494d1fa31d2cf90e21ccc98b777d1ed7a
Ronert Obst <ronert.obst@gmail.com>
2015-02-06 10:45:57 +0100

    add json mode

--- lisp/init-mode-mapping.el
+++ lisp/init-mode-mapping.el
@@ -14,7 +14,7 @@
 ;; JavaScript
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
 (add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
 (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
