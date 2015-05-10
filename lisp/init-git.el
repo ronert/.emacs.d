225f11172aa0f9f2749609e56d7f325092ab102c
Ronert Obst <ronert.obst@gmail.com>
2015-05-10 20:23:39 +0200

    added defun and binding for opening pr
    taken from http://endlessparentheses.com/easily-create-github-prs-from-magit.html?source=rss

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -61,4 +61,21 @@
   :ensure t
   :pin melpa-stable)
 
+;; Open PR
+(defun endless/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/compare/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            (magit-get "remote"
+                       (magit-get-current-remote)
+                       "url"))
+           (magit-get-current-branch))))
+
+(eval-after-load 'magit
+  '(define-key magit-mode-map "V"
+     #'endless/visit-pull-request-url))
+
 (provide 'init-git)
