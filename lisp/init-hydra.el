9715767336462d9bc7604b3a1fdc67ecf9eb3c49
Ronert Obst <ronert.obst@gmail.com>
2015-05-15 21:08:59 +0200

    added more hydras

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -1,11 +1,13 @@
 (use-package hydra
   :ensure t
   :config (progn
+            ;; hydra zoom
             (defhydra hydra-zoom (global-map "<f2>")
               "zoom"
               ("+" text-scale-increase "in")
               ("-" text-scale-decrease "out"))
 
+            ;; hydra git-gutter
             (defhydra hydra-git-gutter (:body-pre (git-gutter-mode 1)
                                                   :hint nil)
               "
@@ -34,6 +36,66 @@ Git gutter:
                           (git-gutter:clear))
                :color blue))
             (global-set-key (kbd "<f10>") 'hydra-git-gutter/body)
+
+            ;; hydra rectangle
+            (defun ora-ex-point-mark ()
+              (interactive)
+              (if rectangle-mark-mode
+                  (exchange-point-and-mark)
+                (let ((mk (mark)))
+                  (rectangle-mark-mode 1)
+                  (goto-char mk))))
+
+            (defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
+                                                 :color pink
+                                                 :post (deactivate-mark))
+              "
+  ^_k_^     _d_elete    _s_tring
+_h_   _l_   _o_k        _y_ank
+  ^_j_^     _n_ew-copy  _r_eset
+^^^^        _e_xchange  _u_ndo
+^^^^        ^ ^         _p_aste
+"
+              ("h" backward-char nil)
+              ("l" forward-char nil)
+              ("k" previous-line nil)
+              ("j" next-line nil)
+              ("e" ora-ex-point-mark nil)
+              ("n" copy-rectangle-as-kill nil)
+              ("d" delete-rectangle nil)
+              ("r" (if (region-active-p)
+                       (deactivate-mark)
+                     (rectangle-mark-mode 1)) nil)
+              ("y" yank-rectangle nil)
+              ("u" undo nil)
+              ("s" string-rectangle nil)
+              ("p" kill-rectangle nil)
+              ("o" nil nil))
+            (global-set-key (kbd "C-x SPC") 'hydra-rectangle/body)
+
+            ;; YASnippet
+            (defhydra hydra-yasnippet (:color blue :hint nil)
+              "
+              ^YASnippets^
+--------------------------------------------
+  Modes:    Load/Visit:    Actions:
+
+ _g_lobal  _d_irectory    _i_nsert
+ _m_inor   _f_ile         _t_ryout
+ _e_xtra   _l_ist         _n_ew
+         _a_ll
+"
+              ("d" yas-load-directory)
+              ("e" yas-activate-extra-mode)
+              ("i" yas-insert-snippet)
+              ("f" yas-visit-snippet-file :color blue)
+              ("n" yas-new-snippet)
+              ("t" yas-tryout-snippet)
+              ("l" yas-describe-tables)
+              ("g" yas/global-mode)
+              ("m" yas/minor-mode)
+              ("a" yas-reload-all))
+            (global-set-key (kbd "C-c C-y") 'hydra-yasnippet/body)
             ))
 
 (provide 'init-hydra)
