7d1dca3e3f2af66fac7c687e8f3fa3c1dc64e490
Ronert Obst <ronert.obst@gmail.com>
2015-05-16 12:28:19 +0200

    add register to guidekey

--- lisp/init-guidekey.el
+++ lisp/init-guidekey.el
@@ -6,7 +6,7 @@
   :config
   (progn
     (guide-key-mode 1)
-    (setq guide-key/guide-key-sequence '("C-x r" "C-x v" "C-c p" "C-x 4" "C-h" "C-y" "C-x 5" "C-c" "C-x" "C-x n"))
+    (setq guide-key/guide-key-sequence '("C-x r" "C-x v" "C-c p" "C-x 4" "C-h" "C-y" "C-x 5" "C-c" "C-x" "C-x n" "C-x C-r"))
 
     (setq guide-key/recursive-key-sequence-flag t)
     (setq guide-key/popup-window-position 'bottom)
