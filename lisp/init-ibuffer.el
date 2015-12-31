a9b589aecde0760eea590604e3ae791695048b11
Ronert Obst <ronert.obst@gmail.com>
2015-12-31 12:15:49 +0100

    explicitly require ibuffer-vc

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -1,58 +1,57 @@
+(require-package 'fullframe)
 (after-load 'ibuffer
-  (fullframe ibuffer ibuffer-quit))
+ (fullframe ibuffer ibuffer-quit))
 
-(use-package ibuffer-vc
-  :ensure t
-  :pin melpa-stable
-  :config (progn
-            (defun ibuffer-set-up-preferred-filters ()
-              (ibuffer-vc-set-filter-groups-by-vc-root)
-              (unless (eq ibuffer-sorting-mode 'filename/process)
-                (ibuffer-do-sort-by-filename/process)))
+(require-package 'ibuffer-vc)
 
-            (add-hook 'ibuffer-hook 'ibuffer-set-up-preferred-filters)
+(defun ibuffer-set-up-preferred-filters ()
+  (ibuffer-vc-set-filter-groups-by-vc-root)
+  (unless (eq ibuffer-sorting-mode 'filename/process)
+    (ibuffer-do-sort-by-filename/process)))
 
-            (setq-default ibuffer-show-empty-filter-groups nil)
+(add-hook 'ibuffer-hook 'ibuffer-set-up-preferred-filters)
+
+(setq-default ibuffer-show-empty-filter-groups nil)
 
 
-            (after-load 'ibuffer
-              ;; Use human readable Size column instead of original one
-              (define-ibuffer-column size-h
-                (:name "Size" :inline t)
-                (cond
-                 ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
-                 ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
-                 (t (format "%8d" (buffer-size))))))
+(after-load 'ibuffer
+  ;; Use human readable Size column instead of original one
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (cond
+     ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+     ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+     (t (format "%8d" (buffer-size))))))
 
 
-            ;; Explicitly require ibuffer-vc to get its column definitions, which
-            ;; can't be autoloaded
-            (after-load 'ibuffer
-              (require 'ibuffer-vc))
+;; Explicitly require ibuffer-vc to get its column definitions, which
+;; can't be autoloaded
+(after-load 'ibuffer
+  (require 'ibuffer-vc))
 
-            ;; Modify the default ibuffer-formats (toggle with `)
-            (setq ibuffer-formats
-                  '((mark modified read-only vc-status-mini " "
-                          (name 18 18 :left :elide)
-                          " "
-                          (size-h 9 -1 :right)
-                          " "
-                          (mode 16 16 :left :elide)
-                          " "
-                          filename-and-process)
-                    (mark modified read-only vc-status-mini " "
-                          (name 18 18 :left :elide)
-                          " "
-                          (size-h 9 -1 :right)
-                          " "
-                          (mode 16 16 :left :elide)
-                          " "
-                          (vc-status 16 16 :left)
-                          " "
-                          filename-and-process)))
+;; Modify the default ibuffer-formats (toggle with `)
+(setq ibuffer-formats
+      '((mark modified read-only vc-status-mini " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)
+        (mark modified read-only vc-status-mini " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              (vc-status 16 16 :left)
+              " "
+              filename-and-process)))
 
-            (setq ibuffer-filter-group-name-face 'font-lock-doc-face)
-            )
-  :bind ("C-x C-b" . ibuffer))
+(setq ibuffer-filter-group-name-face 'font-lock-doc-face)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (provide 'init-ibuffer)
