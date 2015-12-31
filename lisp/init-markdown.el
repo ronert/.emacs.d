7c9ac3ee516335e1b3ce720b262e0de7780f7d1d
Ronert Obst <ronert.obst@gmail.com>
2015-12-31 19:12:24 +0100

    Preview markdown files with Marked 2

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -15,6 +15,18 @@
 (use-package livedown
   :load-path "~/.emacs.d/site-lisp/emacs-livedown")
 
+;; This function will open Marked.app and monitor the current markdown document
+;; for anything changes.  In other words, it will live reload and convert the
+;; markdown documment
+(defun markdown-preview-file ()
+  "run Marked on the current file and revert the buffer"
+  (interactive)
+  (shell-command
+   (format "open -a /Applications/Marked\\ 2.app %s"
+           (shell-quote-argument (buffer-file-name))))
+  )
+(global-set-key "\C-c C-o" 'markdown-preview-file)
+
 ;;; markdown.el ends here
 
 (provide 'init-markdown)
