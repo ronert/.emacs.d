ec3654eab99399ed6a9f190a0ec13c06d9b35800
Ronert Obst <ronert.obst@gmail.com>
2015-12-31 20:48:20 +0100

    Add stan-snippets

--- lisp/init-stan.el
+++ lisp/init-stan.el
@@ -1,5 +1,11 @@
 (use-package stan-mode
   :ensure t
-  :pin melpa-stable)
+  :pin melpa-stable
+  :config
+  (use-package stan-snippets
+    :ensure t
+    :pin melpa-stable
+    )
+  )
 
 (provide 'init-stan)
