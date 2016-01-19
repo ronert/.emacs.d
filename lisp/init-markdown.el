20791a374e372e3c3953b4923eb4eabece6b4eaf
Ronert Obst <ronert.obst@gmail.com>
2016-01-19 17:14:43 +0100

    fixed markdown-preview-file

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -9,20 +9,19 @@
     (setq auto-mode-alist
           (cons '("\\.markdown$" . markdown-mode)
                 (cons '("\\.md$" . markdown-mode) auto-mode-alist)))
-
+    ;; This function will open Marked.app and monitor the current markdown document
+    ;; for anything changes.  In other words, it will live reload and convert the
+    ;; markdown documment
+    (defun markdown-preview-file ()
+      "run Marked on the current file and revert the buffer"
+      (interactive)
+      (shell-command
+       (format "open -a /Applications/Marked\\ 2.app %s"
+               (shell-quote-argument (buffer-file-name))))
+      )
+    (global-set-key (kbd "C-c C-o") 'markdown-preview-file)
     ))
 
-;; This function will open Marked.app and monitor the current markdown document
-;; for anything changes.  In other words, it will live reload and convert the
-;; markdown documment
-(defun markdown-preview-file ()
-  "run Marked on the current file and revert the buffer"
-  (interactive)
-  (shell-command
-   (format "open -a /Applications/Marked\\ 2.app %s"
-           (shell-quote-argument (buffer-file-name))))
-  )
-(global-set-key "\C-c C-o" 'markdown-preview-file)
 
 ;;; markdown.el ends here
 
