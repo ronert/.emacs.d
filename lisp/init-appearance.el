e81028519a5b208d99b9cbf9b97ffbbc58690bae
Ronert Obst <ronert.obst@gmail.com>
2016-02-23 17:38:42 +0100

    fixed grey colour

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -66,7 +66,7 @@
   :config (progn
             (beacon-mode 1)
             (setq beacon-push-mark 35)
-            (setq beacon-color "#666600")))
+            (setq beacon-color "#D3D3De")))
 
 
 
